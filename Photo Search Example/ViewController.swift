c105ddbf83ec2cb998bcdc08f0f36bcd228dd32e
VincentDay <vincent.day@gmail.com>
2015-09-23 17:19:52 -0400

    updated w CocoaPods

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -13,8 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=YOUR_CLIENT_ID",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                print("JSON: " + responseObject.description)
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                print("Error: " + error.localizedDescription)
+        })
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Vince Day on 9/23/15.
+//  Copyright © 2015 Vince Day. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+
+#endif /* bridge_h */
