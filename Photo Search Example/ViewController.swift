13d85625873b2109619ac8493c28f036c9d7154b
VincentDay <vincent.day@gmail.com>
2015-09-23 17:30:50 -0400

    test push

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let manager = AFHTTPRequestOperationManager()
         
-        manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=YOUR_CLIENT_ID",
+        manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=e311dd29d01840f8987e58bc3b31121c",
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
                 print("JSON: " + responseObject.description)
